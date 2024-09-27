56fcd03a8ebc1aaca7a4f86d5071907a7e7c37ff
Ertuğrul Sağdıç <ertugrulsagdic@Ertugruls-MacBook-Pro.local>
2024-09-27 18:25:15 +0300

    mongo conf fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionModule } from './module/question/question.module';
+import { AnswerModule } from './module/answer/answer.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import 'dotenv/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
+    QuestionModule,
+    AnswerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

f051fda7f1fccb0c1b627f1ff1e1f1d3d575e7d6
Ertuğrul Sağdıç <ertugrulsagdic@Ertugruls-MacBook-Pro.local>
2024-09-29 12:05:39 +0300

    controller class end points removed to remove public access of crud operations

--- backend/src/module/user-session/user-session.controller.ts
+++ backend/src/module/user-session/user-session.controller.ts
@@ -1,50 +1,50 @@
 import {
   Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+  // Get,
+  // Post,
+  // Body,
+  // Patch,
+  // Param,
+  // Delete,
 } from '@nestjs/common';
 import { UserSessionService } from './user-session.service';
-import { CreateUserSessionDto } from './dto/create-user-session.dto';
-import { UpdateUserSessionDto } from './dto/update-user-session.dto';
+// import { CreateUserSessionDto } from './dto/create-user-session.dto';
+// import { UpdateUserSessionDto } from './dto/update-user-session.dto';
 
 @Controller('user-session')
 export class UserSessionController {
   constructor(private readonly userSessionService: UserSessionService) {}
 
-  @Post()
-  create(@Body() createUserSessionDto: CreateUserSessionDto) {
-    return this.userSessionService.startSession(createUserSessionDto);
-  }
+  // @Post()
+  // create(@Body() createUserSessionDto: CreateUserSessionDto) {
+  //   return this.userSessionService.startSession(createUserSessionDto);
+  // }
 
-  @Patch(':id/end')
-  endSession(@Param('id') id: string) {
-    return this.userSessionService.endSession(+id);
-  }
+  // @Patch(':id/end')
+  // endSession(@Param('id') id: string) {
+  //   return this.userSessionService.endSession(+id);
+  // }
 
-  @Get()
-  findAll() {
-    return this.userSessionService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.userSessionService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userSessionService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userSessionService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateUserSessionDto: UpdateUserSessionDto,
-  ) {
-    return this.userSessionService.update(+id, updateUserSessionDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateUserSessionDto: UpdateUserSessionDto,
+  // ) {
+  //   return this.userSessionService.update(+id, updateUserSessionDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userSessionService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userSessionService.remove(+id);
+  // }
 }
